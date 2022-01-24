a78673a5ecc974b505460d77e95e87a918ef08f1
Braden Jonsson <braden337@gmail.com>
2022-01-23 21:36:40 -0600

    cache google font

--- src/sw.ts
+++ src/sw.ts
@@ -1,5 +1,5 @@
 import { registerRoute } from 'workbox-routing'
-import { NetworkFirst } from 'workbox-strategies'
+import { CacheFirst, NetworkFirst } from 'workbox-strategies'
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
 
 declare let self: ServiceWorkerGlobalScope
@@ -9,3 +9,7 @@ cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
 
 registerRoute('https://live.railingcorners.com/deaths.json', new NetworkFirst())
+registerRoute(
+  'https://fonts.googleapis.com/css2?family=Source+Code+Pro&family=Source+Sans+Pro:wght@400;700&display=swap',
+  new CacheFirst()
+)
