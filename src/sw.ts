7805a81c7c6b043298333f02f11b6e31ae09df54
Braden Jonsson <braden337@gmail.com>
2022-01-23 21:02:14 -0600

    fetch network first

--- src/sw.ts
+++ src/sw.ts
@@ -1,5 +1,5 @@
 import { registerRoute } from 'workbox-routing'
-import { CacheFirst } from 'workbox-strategies'
+import { CacheFirst, NetworkFirst } from 'workbox-strategies'
 import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
 
@@ -11,7 +11,7 @@ precacheAndRoute(self.__WB_MANIFEST)
 
 registerRoute(
   'https://live.railingcorners.com/deaths.json',
-  new CacheFirst({
+  new NetworkFirst({
     plugins: [
       new CacheableResponsePlugin({
         statuses: [200],
