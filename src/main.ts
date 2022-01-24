ff97ad7198b7bb694286711bc20f3ce0d864f95c
Braden Jonsson <braden337@gmail.com>
2022-01-23 19:48:16 -0600

    register service worker

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
+if ('serviceWorker' in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW()
+}
 
 const app = new App({
   target: document.getElementById('app'),
